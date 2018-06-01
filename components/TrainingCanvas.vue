3e9b112a2397764d163c05c24b412bce38642cbc
André Luiz <andreluiz.97@outlook.com>
2018-05-31 21:06:50 -0300

    Adicionado componente TrainingCanvas com desenho de stage e obstáculos

---
+++ components/TrainingCanvas.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-stage :config="canvas" ref="stage">
+    <v-layer>
+      <v-rect :config="background"/>
+    </v-layer>
+    <v-layer>
+      <v-rect v-for="(obstacle, i) in obstacles" :key="i" :config="obstacle"/>
+    </v-layer>
+  </v-stage>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        canvas: {
+          width: 1000,
+          height: 500
+        },
+
+        obstacles: [
+          { x: 50, y: 60, width: 100, height: 20, fill: 'black' },
+          { x: 200, y: 300, width: 50, height: 30, fill: 'black' },
+          { x: 900, y: 300, width: 20, height: 100, fill: 'black' },
+          { x: 500, y: 200, width: 30, height: 90, fill: 'black' },
+          { x: 100, y: 350, width: 30, height: 30, fill: 'black' },
+          { x: 800, y: 30, width: 40, height: 90, fill: 'black' },
+          { x: 500, y: 400, width: 50, height: 90, fill: 'black' },
+          { x: 500, y: 40, width: 50, height: 20, fill: 'black' },
+          { x: 200, y: 150, width: 50, height: 70, fill: 'black' },
+          { x: 700, y: 250, width: 100, height: 100, fill: 'black' },
+          { x: 250, y: 450, width: 150, height: 30, fill: 'black' }
+        ]
+      }
+    },
+
+    computed: {
+      background () {
+        return {
+          x: 0,
+          y: 0,
+          width: this.canvas.width,
+          height: this.canvas.height,
+          fill: 'white',
+          stroke: 'black'
+        }
+      }
+    }
+  }
+</script>
---
+++ pages/training.vue
@@ -0,0 +1,22 @@
+<template>
+  <container>
+
+    <page-title>Training</page-title>
+    <training-canvas/>
+
+  </container>
+</template>
+
+<script>
+  import Container from '~/components/Container.vue'
+  import PageTitle from '~/components/PageTitle.vue'
+  import TrainingCanvas from '~/components/TrainingCanvas.vue'
+
+  export default {
+    components: {
+      Container,
+      PageTitle,
+      TrainingCanvas
+    }
+  }
+</script>
