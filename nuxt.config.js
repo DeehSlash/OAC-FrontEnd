2a59b6be703b98dd73d02d9059bbd43fb0d07eda
André Luiz <andreluiz.97@outlook.com>
2018-06-02 18:02:35 -0300

    Alterado título da página e cor da barra de progresso

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'oac-frontend',
+    title: 'OAC',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Obstacle Avoiding Car - Front End' }
+      { hid: 'description', name: 'description', content: 'Obstacle Avoiding Car' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -16,7 +16,7 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#1F74FF' },
   /*
   ** Plugins
   */
