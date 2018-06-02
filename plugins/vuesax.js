13e02801d7ac839b51d5654b6d7bfad0fc4edcbb
André Luiz <andreluiz.97@outlook.com>
2018-06-02 18:48:42 -0300

    Adicionadas cores do tema do Vuesax

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -2,4 +2,14 @@ import Vue from 'vue'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //vuesax styles
 
-Vue.use(Vuesax)
+Vue.use(Vuesax, {
+  theme: {
+    colors: {
+      primary:'rgb(31, 116, 255)',
+      success:'rgb(70, 201, 58)',
+      danger:'rgb(255, 71, 87)',
+      warning:'rgb(255, 186, 0)',
+      dark:'rgb(30, 30, 30)'
+    }
+  }
+})
