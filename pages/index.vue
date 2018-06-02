b05687fd328667095aefd1550a32d96a0bdf0a96
André Luiz <andreluiz.97@outlook.com>
2018-06-02 18:22:41 -0300

    Adicionado botão Sobre e adicionada nota no rodapé da página inicial

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,18 @@
     <h4>Obstacle Avoiding Car</h4>
 
     <nuxt-link to="/training">
-      <vs-button vs-type="primary-gradient" vs-icon="play_arrow" vs-width="200px" href="/teste">Start Training</vs-button>
+      <vs-button vs-type="primary-gradient" vs-icon="play_arrow" vs-width="200px">Start Training</vs-button>
     </nuxt-link>
-  
+
+    <nuxt-link to="/about">
+      <vs-button vs-type="dark-gradient" vs-icon="info" vs-width="200px">About</vs-button>
+    </nuxt-link>
+
+    <div class="note">
+      <b>Note:</b> this application is targeted for <b>Desktop</b> only.<br>
+      A minimum screen resolution of <b>1366 x 768</b> is recommended.
+    </div>
+
   </container>
 </template>
 
@@ -18,6 +27,13 @@
 
   h4 {
     margin-bottom: 25px;
+  }
+
+  .note {
+    margin-top: 25px;
+    font-style: italic;
+    font-size: 14px;
+    line-height: 1.25rem;
   }
 </style>
 
