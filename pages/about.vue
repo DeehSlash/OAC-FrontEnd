bb03ee9e17c7d957259fbf75526b854b7b7ec3ff
André Luiz <andreluiz.97@outlook.com>
2018-06-02 18:40:40 -0300

    Correção de identação na página Sobre

--- pages/about.vue
+++ pages/about.vue
@@ -1,22 +1,22 @@
 <template>
   <container>
   
-  <page-title>About</page-title>
+    <page-title>About</page-title>
 
-  <div class="about">
-    This project has been developed by <b>André Luiz</b> and <b>Alberto Neto</b>.<br><br>
-    GitHub repositories:<br><br>
-    <a href="https://github.com/DeehSlash/OAC-FrontEnd" target="_blank">
-      <vs-button vs-type="primary-line-down">OAC-FrontEnd</vs-button>
-    </a>
-    <a href="https://github.com/DeehSlash/OAC-API" target="_blank">
-      <vs-button vs-type="primary-line-down">OAC-API</vs-button>
-    </a>
-  </div>
+    <div class="about">
+      This project has been developed by <b>André Luiz</b> and <b>Alberto Neto</b>.<br><br>
+      GitHub repositories:<br><br>
+      <a href="https://github.com/DeehSlash/OAC-FrontEnd" target="_blank">
+        <vs-button vs-type="primary-line-down">OAC-FrontEnd</vs-button>
+      </a>
+      <a href="https://github.com/DeehSlash/OAC-API" target="_blank">
+        <vs-button vs-type="primary-line-down">OAC-API</vs-button>
+      </a>
+    </div>
 
-  <div class="footer">
-    <a href="https://www.univali.br" target="_blank">UNIVALI - Universidade do Vale do Itajaí</a>
-  </div>
+    <div class="footer">
+      <a href="https://www.univali.br" target="_blank">UNIVALI - Universidade do Vale do Itajaí</a>
+    </div>
 
   </container>
 </template>
