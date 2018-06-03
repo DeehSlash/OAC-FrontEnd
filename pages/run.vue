0baf3412858bfa5470b41e802b6a0042fd2c8e20
André Luiz <andreluiz.97@outlook.com>
2018-06-03 14:46:41 -0300

    Página de execução

---
+++ pages/run.vue
@@ -0,0 +1,22 @@
+<template>
+  <container>
+
+    <page-title>Execution</page-title>
+    <execution-canvas/>
+
+  </container>
+</template>
+
+<script>
+  import Container from '~/components/Container.vue'
+  import PageTitle from '~/components/PageTitle.vue'
+  import ExecutionCanvas from '~/components/ExecutionCanvas.vue'
+
+  export default {
+    components: {
+      Container,
+      PageTitle,
+      ExecutionCanvas
+    }
+  }
+</script>
