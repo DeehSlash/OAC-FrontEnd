9884f234b222895ca2d7c9425734dd716201a321
André Luiz <andreluiz.97@outlook.com>
2018-06-03 00:11:35 -0300

    Adicionado comentário de aviso no plugin Vuesax

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //vuesax styles
 
+// ! Setting theme with SSR causes a document is not defined bug. Adding the condition below temporarily corrects it, but it's not the correct way to solve it. Check this issue for more details: https://github.com/lusaxweb/vuesax/issues/83
 if (process.browser) {
   Vue.use(Vuesax, {
     server: true,
