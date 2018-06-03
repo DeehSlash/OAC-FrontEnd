8c46b4911e4b9d6c45317d42659a64260f8918d1
André Luiz <andreluiz.97@outlook.com>
2018-06-03 18:01:52 -0300

    Correção temporária para bug de tema do Vuesax

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,3 +8,17 @@
     height: 100%;
   }
 </style>
+
+<script>
+  export default {
+    mounted () {
+      this.$vs.theme({
+        primary:'rgb(31, 116, 255)',
+        success:'rgb(70, 201, 58)',
+        danger:'rgb(255, 71, 87)',
+        warning:'rgb(255, 186, 0)',
+        dark:'rgb(30, 30, 30)'
+      })
+    }
+  }
+</script>
--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -3,17 +3,4 @@ import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //vuesax styles
 
 // ! Setting theme with SSR causes a document is not defined bug. Adding the condition below temporarily corrects it, but it's not the correct way to solve it. Check this issue for more details: https://github.com/lusaxweb/vuesax/issues/83
-if (process.browser) {
-  Vue.use(Vuesax, {
-    server: true,
-    theme: {
-      colors: {
-        primary:'rgb(31, 116, 255)',
-        success:'rgb(70, 201, 58)',
-        danger:'rgb(255, 71, 87)',
-        warning:'rgb(255, 186, 0)',
-        dark:'rgb(30, 30, 30)'
-      }
-    }
-  })
-}
+Vue.use(Vuesax)
