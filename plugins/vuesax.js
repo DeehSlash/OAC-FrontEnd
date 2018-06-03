7027b3042689bdaf686469b523af0f5fa02dcf18
André Luiz <andreluiz.97@outlook.com>
2018-06-03 00:11:17 -0300

    Correção temporária em bug do Vuesax com SSR

--- plugins/vuesax.js
+++ plugins/vuesax.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //vuesax styles
 
-Vue.use(Vuesax, {
-  theme: {
-    colors: {
-      primary:'rgb(31, 116, 255)',
-      success:'rgb(70, 201, 58)',
-      danger:'rgb(255, 71, 87)',
-      warning:'rgb(255, 186, 0)',
-      dark:'rgb(30, 30, 30)'
+if (process.browser) {
+  Vue.use(Vuesax, {
+    server: true,
+    theme: {
+      colors: {
+        primary:'rgb(31, 116, 255)',
+        success:'rgb(70, 201, 58)',
+        danger:'rgb(255, 71, 87)',
+        warning:'rgb(255, 186, 0)',
+        dark:'rgb(30, 30, 30)'
+      }
     }
-  }
-})
+  })
+}
